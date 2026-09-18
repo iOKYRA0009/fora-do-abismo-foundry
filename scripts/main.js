0557dd92df59fa94bc1b06ef6607b2224f4e459c
iOKYRA0009 <rogeraz0009@gmail.com>
2026-09-18 08:08:58 -0300

    feat: expose appearance choreography status

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import { JarvisImporterV9 } from "./importer/v9/jarvis-importer-v9.js";
 import { registerHybridFeatureLayout } from "./ui/hybrid-features.js";
 import { registerMacroFileDrop } from "./ui/macro-file-drop.js";
 import { registerActivityAutomation } from "./automation/activity-automation.js";
-import { registerAppearanceEffectAutomation } from "./automation/appearance-effect-automation.js";
+import { getAppearanceChoreographyStatus, registerAppearanceEffectAutomation } from "./automation/appearance-effect-automation.js";
 import {
   applyEmbeddedEffect,
   applyEmbeddedEffectToActor,
@@ -113,7 +113,8 @@ Hooks.once("ready", () => {
       audioEffect: createAudioEffect,
       installLocal: installLocalAppearance,
       installPack: installAppearancePack,
-      importVisageExport
+      importVisageExport,
+      choreographyStatus: getAppearanceChoreographyStatus
     },
     transformation: {
       status: getTransformationStatus,
